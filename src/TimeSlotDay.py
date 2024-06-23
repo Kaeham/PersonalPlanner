cafff1a4fc2ddaddc92718d1d1d7b5e5a59123cc
Kaeham <106434990+Kaeham@users.noreply.github.com>
2024-06-23 14:54:54 +1000

    added basic classes representing time slots in the planner

---
+++ src/TimeSlotDay.py
@@ -0,0 +1,16 @@
+from datetime import * 
+
+class TimeSlotDay:
+    tasks = []
+
+    def __init__(self, ) -> None:
+
+    def add_list(self, list_to_add) -> None:
+        pass
+
+    def delete_list(self) -> None:
+        pass
+
+    def change_list_order(self) -> None:
+        pass
+    
