bb58b0d6426f1f197a6a96f818f100ad2be0abe2
Kaeham <106434990+Kaeham@users.noreply.github.com>
2024-06-29 12:38:36 +1000

    added __str__ method for testing purposes

--- src/TimeSlotDay.py
+++ src/TimeSlotDay.py
@@ -22,9 +22,15 @@ class TimeSlotDay:
     def change_list_order(self, task_one, task_two) -> None:
         pass
 
+    def __str__(self) -> str:
+        res = ""
+        res += self.date.strftime("%d %A") + "\n"
+        for task in self.tasks:
+            res+= str(task) + "\n"
+        return res
+
 today = date.today()
-d3 = today.strftime("%A")
-d1 = today.strftime("%d")
-d2 = today.strftime("%m")
-print(d3, d1, d2)
-print(today)
+d3 = today.strftime("%A") # weekday in str form
+d1 = today.strftime("%d") # day of month
+d2 = today.strftime("%m") # month in int form
+# print(today)
--- src/TimeSlotWeek.py
+++ src/TimeSlotWeek.py
@@ -1,18 +1,33 @@
 from TimeSlotDay import TimeSlotDay
 from datetime import date, timedelta
 
+#testing imports
+from PlannerList import PlannerList
+from ListItem import ListItem
+
 class TimeSlotWeek:
     def __init__(self, epoch_date) -> None:
         self.days = []
         epochWeekday = int(epoch_date.strftime("%w"))
-        epoch_date -= timedelta(days=epochWeekday)
+        epoch_date -= timedelta(days=epochWeekday + 1)
 
-        for x in range(7):
-            epoch_date += timedelta(days=x)
+        for _ in range(7):
+            epoch_date += timedelta(days=1)
             self.days.append(TimeSlotDay(epoch_date))
+    
+    def __str__(self) -> str:
+        res = ""
+        for day in self.days:
+            res += str(day) + " "
+        return res
 
 today = date.today()
-epochWeekday = int(today.strftime("%w"))
-today -= timedelta(days=epochWeekday)
-print(today)
+test = TimeSlotWeek(today)
+test_ = test.days[0]
+
+taskOneItemOne = ListItem("testOne", "Test for Item One")
+taskOne = PlannerList("testOne", test_)
+taskOne.add_item(taskOneItemOne)
+test_.add_task(taskOne)
+print(test_)
 
